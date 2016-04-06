8cc3394443c316d6da98d222b6a4f47ef58eb93c
koktoh <koktoh96@gmail.com>
2016-04-07 04:09:09 +0900

    もろもろの概形を作成

---
+++ ProxyManager/Model/ConfigOperator.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace ProxyManager.Model
+{
+	public class ConfigOperator
+	{
+		private string _savePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "ProxyManager");
+		private string _configFile;
+
+		private List<ConfigData> _configDataList = new List<ConfigData>();
+
+		public List<ConfigData> ConfigDataList { get; set; }
+
+		public ConfigOperator()
+		{
+			if (!Directory.Exists(_savePath))
+			{
+				Directory.CreateDirectory(_savePath);
+			}
+
+			_configFile = Path.Combine(_savePath, "config.json");
+		}
+
+		public void SaveConfig()
+		{
+			using (var sw = new StreamWriter(_configFile, false))
+			{
+				sw.Write(JsonConvert.SerializeObject(ConfigDataList));
+			}
+		}
+
+		public void LoadConfig()
+		{
+			using (var sr = new StreamReader(_configFile))
+			{
+				ConfigDataList = JsonConvert.DeserializeObject<List<ConfigData>>(sr.ReadToEnd());
+			}
+		}
+	}
+}
---
+++ ProxyManager/ViewModel/ProxyViewModel.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace ProxyManager.ViewModel
+{
+	public class ProxyViewModel : INotifyPropertyChanged
+	{
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		private string _name;
+		private string _device;
+		private string _proxy;
+		private string _port;
+		private string _authName;
+		private string _authPassword;
+
+		public string Name
+		{
+			get
+			{
+				return _name;
+			}
+
+			set
+			{
+				_name = value;
+				RaisePropertyChanged("Name");
+			}
+		}
+
+		public string Device
+		{
+			get
+			{
+				return _device;
+			}
+
+			set
+			{
+				_device = value;
+				RaisePropertyChanged("Device");
+			}
+		}
+
+		public string Proxy
+		{
+			get
+			{
+				return _proxy;
+			}
+
+			set
+			{
+				_proxy = value;
+				RaisePropertyChanged("Proxy");
+			}
+		}
+
+		public string Port
+		{
+			get
+			{
+				return _port;
+			}
+
+			set
+			{
+				_port = value;
+				RaisePropertyChanged("Port");
+			}
+		}
+
+		public string AuthName
+		{
+			get
+			{
+				return _authName;
+			}
+
+			set
+			{
+				_authName = value;
+				RaisePropertyChanged("AuthName");
+			}
+		}
+
+		public string AuthPassword
+		{
+			get
+			{
+				return _authPassword;
+			}
+
+			set
+			{
+				var str = new string('*', value.Length);
+				_authPassword = str;
+				RaisePropertyChanged("AuthPassword");
+			}
+		}
+
+
+
+		protected virtual void RaisePropertyChanged(string propertyName)
+		{
+			if (PropertyChanged != null)
+			{
+				PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+			}
+		}
+	}
+}
